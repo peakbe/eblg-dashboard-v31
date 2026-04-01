bab2c9ca2fb75d3a3b29c0e504310a83bad960eb
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-04-01 13:53:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import { initMap } from "./map.js";
-import { initUI } from "./ui.js";
-import { loadMetar } from "./metar.js";
-import { loadTaf } from "./taf.js";
-import { loadFids } from "./fids.js";
+import { initMap } from "./js/map.js";
+import { initUI } from "./js/ui.js";
+import { loadMetar } from "./js/metar.js";
+import { loadTaf } from "./js/taf.js";
+import { loadFids } from "./js/fids.js";
 
 window.onload = () => {
     window.map = initMap();
